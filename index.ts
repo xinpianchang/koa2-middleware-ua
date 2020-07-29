7cad061d5dc7a90a9ab0cb357f27414a779267b4
tanghongyu <505648237@qq.com>
2020-07-29 11:16:24 +0800

    take out  console

--- index.ts
+++ index.ts
@@ -8,7 +8,6 @@ import UserAgent from './lib/useragent'
 
 export const userAgent: Middleware  = async (ctx: ParameterizedContext, next: Next): Promise<void> => {
   const originUA = ctx.header['user-agent']
-  console.log('ctx', ctx)
   ctx.ua = new UserAgent(originUA)
   await next()
 }
